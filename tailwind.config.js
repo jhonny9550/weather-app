0503138d90aec3442dc656da1e0b771f63cb9364
jhonny9550 <jhonny9550@gmail.com>
2022-02-12 08:09:51 +1100

    Initial project config added
    Tailwind setup
    
    Unused files removed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {
+      colors: {
+        base: '#100E1D',
+        paper: '#1E213A',
+        contrast: '#E7E7EB',
+        primary: '#6E707A',
+        secondary: '#3C47E9'
+      }
+    },
+  },
+  plugins: [],
+}
